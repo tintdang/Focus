0f1cc2deaec279509e48e8d075d75481c189fad8
WhoisJohnKim <johnkim9417@gmail.com>
2018-09-11 13:14:52 -0500

    Spotify is working

--- johnKim/spotify.js
+++ johnKim/spotify.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
-var request = require('ajax-request');
+var spotify = new Spotify(keys.spotify);;
 var song = process.argv.slice(2).join(" ");
 var fetch = require('node-fetch');
 
