5a51befc123f32be7ebf4c04a4da02036f95c74a
TDang <tin2dang@gmail.com>
2018-09-12 14:30:13 -0500

    moved signup and login links

--- public/js/theAwesomeTimer.js
+++ public/js/theAwesomeTimer.js
@@ -25,21 +25,10 @@ function focusTimer(duration) {
         if (--timer < 0) {
             stop(count);
             $("#focus-timer").hide();
-            // $.get("/break", function(data) {
-            //     console.log(data);
-            // });
-            // var nextPage = window.location.hostname + "/break";
             window.location.replace("/break");
         }
-<<<<<<< HEAD
-
     }, 1000);
 }
-
-=======
-    }, 1000);
-}
->>>>>>> 171d16d8bcd2af17aaf46b2b1e491a622be22a19
 
 ///////////////////////////////////////////////////////////////
 // SPOTIFY ADD SONG
@@ -57,38 +46,3 @@ spotifyForm.on("click", function(event){
         });
     }
 });
-<<<<<<< HEAD
-
-function breakTimer(duration) {
-    var display = document.querySelector("#break-time");
-    // var displayTime = "17:00"; May use this for displaying timer
-    var timer = duration, minutes, seconds;
-    var count = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        // display.textContent = displayTime;
-        // setTimeout(function () {
-        display.textContent = minutes + ":" + seconds;
-        // }, 1000);
-
-        if (--timer < 0) {
-            stop(count);
-            $("#break-timer").hide();
-            $("#start-page").show();
-        }
-    }, 1000);
-
-}
-
-///////////////////////////////////////////////////////////////
-// STOP TIMER
-///////////////////////////////////////////////////////////////
-function stop(clearTimer) {
-    clearInterval(clearTimer);
-}
-=======
->>>>>>> 171d16d8bcd2af17aaf46b2b1e491a622be22a19
